b325f97c130be1fc24d52a6c0ef3b1dfa91a13c5
EyreFree <zheng.xu03@ele.me>
2017-09-05 20:30:38 +0800

    Remove useless code

--- EFMarkdown/Classes/String+.swift
+++ EFMarkdown/Classes/String+.swift
@@ -10,15 +10,17 @@ import UIKit
 
 extension String {
 
+    // [] 操作符重载
     subscript(index: Int) -> Character {
         return self[self.index(self.startIndex, offsetBy: index)]
     }
 
+    // 字符数量
     func count() -> Int {
         return self.characters.count
     }
 
-    //正则表达式
+    // 是否符合输入的正则表达式
     func conform(regex: String) -> Bool {
         return NSPredicate(format: "SELF MATCHES %@", regex).evaluate(with: self)
     }
@@ -28,7 +30,7 @@ extension String {
         return self.components(separatedBy: subString).count - 1
     }
 
-    //替换某个子字符串为另一字符串
+    // 替换某个子字符串为另一字符串
     func replace(_ string: String, with: String, options: CompareOptions? = nil) -> String {
         if let options = options {
             return self.replacingOccurrences(of: string, with: with, options: options, range: nil)
@@ -36,7 +38,7 @@ extension String {
         return self.replacingOccurrences(of: string, with: with)
     }
 
-    //替换前缀
+    // 替换前缀
     func replacePrefix(string: String, with: String) -> String {
         if self.hasPrefix(string) {
             return with + String(self.characters.dropFirst(string.count()))
@@ -44,7 +46,7 @@ extension String {
         return self
     }
 
-    //替换尾缀
+    // 替换尾缀
     func replaceSuffix(string: String, with: String) -> String {
         if self.hasSuffix(string) {
             return String(self.characters.dropLast(string.count())) + with
@@ -52,24 +54,19 @@ extension String {
         return self
     }
 
-    //移除某个子串
+    // 移除某个子串
     func remove(string: String) -> String {
         return self.replace(string, with: "")
     }
 
-    //移除某个前缀
+    // 移除某个前缀
     func removePrefix(string: String) -> String {
         return self.replacePrefix(string: string, with: "")
     }
 
-    //移除某个尾缀
+    // 移除某个尾缀
     func removeSuffix(string: String) -> String {
         return self.replaceSuffix(string: string, with: "")
-    }
-
-    //去除左右空格和换行
-    func clean() -> String {
-        return self.trimmingCharacters(in: NSCharacterSet.whitespacesAndNewlines)
     }
 
     // 将多个连续重复字符变为一个
@@ -82,43 +79,5 @@ extension String {
             }
         }
         return outString
-    }
-
-    // 是否由 chars 所包含的字符组成
-    func isConsistof(chars: String) -> Bool {
-        for index in 0 ..< self.count() {
-            var mark = false
-            for joke in 0 ..< chars.count() {
-                if self[index] == chars[joke] {
-                    mark = true
-                    break
-                }
-            }
-            if false == mark {
-                return false
-            }
-        }
-        return true
-    }
-
-    static func isEmpty(string: String?) -> Bool {
-        if let tryString = string {
-            return tryString == ""
-        }
-        return true
-    }
-
-    // 获取标签内容
-    func elementContent(make: String, markOver: String? = nil) -> String {
-        let beginMark = make
-        let endMark = markOver ?? make.replace("<", with: "</")
-
-        if let block1 = ("2333" + self).components(separatedBy: beginMark).dropFirst(1).first {
-            if block1.hasPrefix(endMark) {
-                return ""
-            }
-            return block1.components(separatedBy: endMark).first ?? ""
-        }
-        return ""
     }
 }
